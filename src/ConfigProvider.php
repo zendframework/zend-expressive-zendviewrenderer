58937d54c543846559dbdc37e4dd267e3e12f5f0
webimpress <contact@webimpress.com>
2017-12-13 14:34:50 +0000

    Added ConfigProvider with basic configuration

---
+++ src/ConfigProvider.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @see       https://github.com/zendframework/zend-expressive-zendviewrenderer for the canonical source repository
+ * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (https://www.zend.com)
+ * @license   https://github.com/zendframework/zend-expressive-zendviewrenderer/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace Zend\Expressive\ZendView;
+
+use Zend\Expressive\Template\TemplateRendererInterface;
+use Zend\View\HelperPluginManager;
+
+class ConfigProvider
+{
+    public function __invoke() : array
+    {
+        return [
+            'dependencies' => $this->getDependencies(),
+            'templates' => $this->getTemplates(),
+        ];
+    }
+
+    public function getDependencies() : array
+    {
+        return [
+            'aliases' => [
+                TemplateRendererInterface::class => ZendViewRenderer::class,
+            ],
+            'factories' => [
+                HelperPluginManager::class => HelperPluginManagerFactory::class,
+                ZendViewRenderer::class => ZendViewRendererFactory::class,
+            ],
+        ];
+    }
+
+    public function getTemplates() : array
+    {
+        return [
+            'extension' => 'phtml',
+            'paths' => [],
+        ];
+    }
+}
