80681592f23b99ba08b7059aa670f2f8e9c42f4f
Aleksei Khudiakov <aleksey@xerkus.pro>
2019-06-04 13:58:22 +1000

    2.2.0 readiness

---
+++ src/NamespacedPathStackResolverFactory.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @see       https://github.com/zendframework/zend-expressive-zendviewrenderer for the canonical source repository
+ * @copyright Copyright (c) 2019 Zend Technologies USA Inc. (https://www.zend.com)
+ * @license   https://github.com/zendframework/zend-expressive-zendviewrenderer/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace Zend\Expressive\ZendView;
+
+use Psr\Container\ContainerInterface;
+
+class NamespacedPathStackResolverFactory
+{
+    public function __invoke(ContainerInterface $container) : NamespacedPathStackResolver
+    {
+        $config   = $container->has('config') ? $container->get('config') : [];
+        $config   = $config['templates'] ?? [];
+
+        $resolver = new NamespacedPathStackResolver();
+        if (! empty($config['default_suffix'])) {
+            $resolver->setDefaultSuffix($config['default_suffix']);
+        }
+
+        return $resolver;
+    }
+}
